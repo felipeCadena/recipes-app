7167a223813682071fe0bee69fe05896c601c886
felipeCadena <felipecaden@gmail.com>
2023-09-20 18:59:46 -0300

    corrigindo lint nos teste

--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -4,15 +4,12 @@ import userEvent from '@testing-library/user-event';
 import Login from '../components/Login';
 import { renderWithRouter } from './helpers/renderWithRouter';
 
-describe('Verifica existência dos elementos do componente Login', () => {
-  let emailInput: HTMLElement, passwordInput: HTMLElement, submitButton: HTMLElement;
+const email = 'email-input';
+const password = 'password-input';
+const loginBtn = 'login-submit-btn';
 
-  beforeEach(() => {
-    const { getByTestId, getByText } = renderWithRouter(<Login />);
-    emailInput = getByTestId('email-input');
-    passwordInput = getByTestId('password-input');
-    submitButton = getByText('Enter');
-  });
+describe('Verifica existência dos elementos do componente Login', () => {
+  const { getByTestId, getByText } = renderWithRouter(<Login />);
 
   it('Verifica se o componente Login é renderizado', () => {
     const loginTitle = screen.getByRole('heading', { name: /login/i });
@@ -20,24 +17,23 @@ describe('Verifica existência dos elementos do componente Login', () => {
   });
 
   it('Verifica se o componente Login possui 2 inputs', () => {
+    const emailInput = getByTestId(email);
+    const passwordInput = getByTestId(password);
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
   });
 
   it('Verifica se o componente Login possui 1 botão', () => {
+    const submitButton = getByText('Enter');
     expect(submitButton).toBeInTheDocument();
   });
 });
 
 describe('Verifica se os data-testid estão corretos no componente Login', () => {
-  let emailInput: HTMLElement, passwordInput: HTMLElement, submitButton: HTMLElement;
-
-  beforeEach(() => {
-    const { getByTestId } = renderWithRouter(<Login />);
-    emailInput = getByTestId('email-input');
-    passwordInput = getByTestId('password-input');
-    submitButton = getByTestId('login-submit-btn');
-  });
+  const { getByTestId } = renderWithRouter(<Login />);
+  const emailInput = screen.getByTestId(email);
+  const passwordInput = getByTestId(password);
+  const submitButton = getByTestId(loginBtn);
 
   it('Verifica o atributo data-testid dos elementos de Login', () => {
     expect(emailInput).toBeInTheDocument();
@@ -47,14 +43,15 @@ describe('Verifica se os data-testid estão corretos no componente Login', () =>
 });
 
 describe('Verifica se o usuário pode preencher os campos de input no componente Login', () => {
+  const emailTeste = 'trybe@email.com';
   it('Verifica se o usuário escreve o e-mail no input de e-mail', () => {
-    const emailInput = screen.getByTestId('email-input') as HTMLInputElement;
-    userEvent.type(emailInput, 'trybe@email.com');
-    expect(emailInput.value).toEqual('trybe@email.com');
+    const emailInput = screen.getByTestId(email) as HTMLInputElement;
+    userEvent.type(emailInput, emailTeste);
+    expect(emailInput.value).toEqual(emailTeste);
   });
 
   it('Verifica se o usuário pode escrever a senha no input de senha', () => {
-    const passwordInput = screen.getByTestId('password-input') as HTMLInputElement;
+    const passwordInput = screen.getByTestId(password) as HTMLInputElement;
     userEvent.type(passwordInput, 'minhasenha');
     expect(passwordInput.value).toEqual('minhasenha');
   });
@@ -62,8 +59,8 @@ describe('Verifica se o usuário pode preencher os campos de input no componente
 
 describe('Verifica se o formulário só fica válido após e-mail válido e senha com mais de 6 caracteres', () => {
   it('O botão de login só fica válido após e-mail e senha válidos', () => {
-    const emailInput = screen.getByTestId('email-input') as HTMLInputElement;
-    const passwordInput = screen.getByTestId('password-input') as HTMLInputElement;
+    const emailInput = screen.getByTestId(email) as HTMLInputElement;
+    const passwordInput = screen.getByTestId(password) as HTMLInputElement;
     const submitButton = screen.getByText('Enter', { selector: 'button' }) as HTMLButtonElement;
     expect(submitButton).toBeDisabled();
     userEvent.type(emailInput, 'trybe@email.com');
@@ -75,27 +72,30 @@ describe('Verifica se o formulário só fica válido após e-mail válido e senh
 
 describe('Verifica se salva o e-mail da pessoa usuária em localStorage após a submissão', () => {
   it('O e-mail da pessoa usuária deve ser salvo no localStorage após a submissão', () => {
+    const emailTeste = 'meuemail@exemplo.com';
+
     const emailInput = screen.getByTestId('email-input') as HTMLInputElement;
     const passwordInput = screen.getByTestId('password-input') as HTMLInputElement;
-    const loginSubmitBtn = screen.getByTestId('login-submit-btn');
-    userEvent.type(emailInput, 'meuemail@exemplo.com');
+    const loginSubmitBtn = screen.getByTestId(loginBtn);
+    userEvent.type(emailInput, emailTeste);
     userEvent.type(passwordInput, 'minhasenha');
     userEvent.click(loginSubmitBtn);
-    expect(localStorage.getItem('user')).toEqual(JSON.stringify({ email: 'meuemail@exemplo.com' }));
+    expect(localStorage.getItem('user')).toEqual(JSON.stringify({ email: emailTeste }));
   });
 });
 
 describe('Verifica o redirecionamento após a submissão e cobertura de Login', () => {
   it('Verifica se redireciona a pessoa usuária para a tela principal de receitas de comidas após o login', async () => {
-    const emailInput = screen.getByTestId('email-input') as HTMLInputElement;
-    const passwordInput = screen.getByTestId('password-input') as HTMLInputElement;
-    const loginSubmitBtn = screen.getByTestId('login-submit-btn');
-    userEvent.type(emailInput, 'meuemail@exemplo.com');
+    const emailTeste = 'meuemail@exemplo.com';
+    const emailInput = screen.getByTestId(email) as HTMLInputElement;
+    const passwordInput = screen.getByTestId(password) as HTMLInputElement;
+    const loginSubmitBtn = screen.getByTestId(loginBtn);
+    userEvent.type(emailInput, emailTeste);
     userEvent.type(passwordInput, 'minhasenha');
     userEvent.click(loginSubmitBtn);
     await waitFor(() => {
       expect(window.location.pathname).toEqual('/meals');
-      expect(localStorage.getItem('user')).toEqual(JSON.stringify({ email: 'meuemail@exemplo.com' }));
+      expect(localStorage.getItem('user')).toEqual(JSON.stringify({ email: emailTeste }));
     });
   });
 });
