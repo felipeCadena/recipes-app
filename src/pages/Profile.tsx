26cc7754827ab1b7f63cd45467cf6406c4346bce
felipeCadena <felipecaden@gmail.com>
2023-09-25 19:11:47 -0300

    reqs 57 a 62

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 function Profile() {
+  const [user, setUser] = useState('');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      const { email } = JSON.parse(localStorage.getItem('user') as string);
+      setUser(email);
+    }
+  }, []);
+
+  function handleLogout() {
+    localStorage.clear();
+    navigate('/');
+  }
+
   return (
     <div>
-      <h1>Profile</h1>
+      <h2 data-testid="profile-email">{user}</h2>
+      <Link to="/done-recipes">
+        <button data-testid="profile-done-btn">Done Recipes</button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+      </Link>
+      <button onClick={ handleLogout } data-testid="profile-logout-btn">Logout</button>
     </div>
   );
 }
